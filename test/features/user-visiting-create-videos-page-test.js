86898174a09fccb1c198fcfc9e9d37de1748d7b5
1334 <1334@shadowproject.net>
2018-04-16 18:25:02 +0200

    add a feature test to create a new video entry

--- test/features/user-visiting-create-videos-page-test.js
+++ test/features/user-visiting-create-videos-page-test.js
@@ -7,4 +7,19 @@ describe('visiting create videos page', () => {
 
     assert.include(browser.getText('body'), 'Save a video');
   });
+
+  describe('filling the form', () => {
+    it('creates a new video entry', () => {
+      const title = 'Some title';
+      const description = 'Some description';
+      browser.url('/videos/create.html');
+
+      browser.setValue('#title-input', title);
+      browser.setValue('#description-input', description);
+      browser.click('#submit-video-btn');
+
+      assert.include(browser.getText('#videos-container'), title);
+      assert.include(browser.getText('#videos-container'), description);
+    });
+  });
 });
