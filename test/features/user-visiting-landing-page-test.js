37848554144066dd4b8b56f30b3c00544d35c5e5
1334 <1334@shadowproject.net>
2018-04-16 17:31:02 +0200

    write feature for visiting the empty root page and make it pass

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,11 @@
 const {assert} = require('chai');
+
+describe('visiting /', () => {
+  describe('when there are no videos', () => {
+    it('the videos container should be empty', () => {
+      browser.url('/');
+
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+});
