a497dc8e3bd8be3f0a6af76c32a22c43488e1572
1334 <1334@shadowproject.net>
2018-04-17 17:46:56 +0200

    refactor routes to separate file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+const videosRoute = require('./routes/videos');
 
 // View engine setup
 app.engine('handlebars', expressHandlebars({defaultLayout: 'app'}));
@@ -17,10 +18,7 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/videos', (req, res) => {
-  res.status(201).send();
-});
-
+app.use('/', videosRoute);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
