7042032b29153e30eef15e9e98e1f3ae96cc1365
1334 <1334@shadowproject.net>
2018-04-20 19:47:31 +0200

    merge video server tests in a single file

--- test/routes/videos-test.js
+++ test/routes/videos-test.js
@@ -5,15 +5,43 @@ const {connectDatabaseAndDropData, disconnectDatabase} = require('../database-ut
 const app = require('../../app');
 const Video = require('../../models/video');
 
-describe('/videos GET', () => {
+describe('/videos', () => {
 
   beforeEach(connectDatabaseAndDropData);
   afterEach(disconnectDatabase);
 
-  it('can render existing videos', async () => {
-    const video = await Video.create({ title: 'existing title', description: 'existing video description' });
-    const response = await request(app).get('/videos');
+  describe('GET', () => {
+    it('can render existing videos', async () => {
+      const video = await Video.create({ title: 'existing title', description: 'existing video description' });
+      const response = await request(app).get('/videos');
 
-    assert.include(response.text, video.title);
+      assert.include(response.text, video.title);
+    });
+  });
+
+  describe('POST', () => {
+    it('can create a video', async () => {
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send();
+
+      assert.equal(response.status, 201);
+    });
+
+
+    it('saves video to the database', async () => {
+      const title = 'Title to save';
+      const description = 'Description to save';
+
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send({title, description});
+      const createdItem = await Video.findOne({});
+
+      assert.equal(createdItem.title, title);
+      assert.equal(createdItem.description, description);
+    });
   });
 });
