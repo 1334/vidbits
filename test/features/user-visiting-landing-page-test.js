4f8650082a917ef07b87ce935235e783f95b98c5
1334 <1334@shadowproject.net>
2018-04-22 12:44:25 +0200

    add an assertion to verify individual videos are linked form the landing page

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -11,22 +11,29 @@ describe('visiting /', () => {
 
   describe('when there are some videos created', () => {
     it('it display the information of the created videos', () => {
-      const title = 'Some existing title';
-      const description = 'Some existing description';
-      const videoUrl = generateRandomUrl('example.com');
-
-      browser.url('/videos/create');
-      browser.setValue('#url-input', videoUrl);
-      browser.setValue('#title-input', title);
-      browser.setValue('#description-input', description);
-      browser.click('#submit-video-btn');
-      browser.url('/');
+      const { title, description, url } = createVideo();
 
       assert.include(browser.getText('#videos-container'), title);
-      assert.include(browser.getAttribute('iframe.video-player', 'src'), videoUrl);
+      assert.include(browser.getAttribute('iframe.video-player', 'src'), url);
+      assert.include(browser.getAttribute('.video-title a', 'href'), '/videos/');
     });
   });
 });
+
+const createVideo = (options = {}) => {
+  const title = options.title || 'Some existing title';
+  const description = options.description || 'Some existing description';
+  const url = options.url || generateRandomUrl('example.com');
+
+  browser.url('/videos/create');
+  browser.setValue('#url-input', url);
+  browser.setValue('#title-input', title);
+  browser.setValue('#description-input', description);
+  browser.click('#submit-video-btn');
+  browser.click('.title-logo a');
+
+  return { title, description, url };
+};
 
 const generateRandomUrl = (domain) => {
   return `http://${domain}/${Math.random()}`;
