e911851076d5293cef6aadc053a45a581aadf381
1334 <1334@shadowproject.net>
2018-04-22 13:45:36 +0200

    now the show plays embeds video in an iframe

--- test/routes/videos-show-test.js
+++ test/routes/videos-show-test.js
@@ -1,6 +1,7 @@
 const {assert} = require('chai');
 const request = require('supertest');
 const {connectDatabaseAndDropData, disconnectDatabase} = require('../database-utilities');
+const {jsdom} = require('jsdom');
 const {parseTextFromHTML} = require('../test-utils.js');
 const app = require('../../app');
 const Video = require('../../models/video');
@@ -10,9 +11,11 @@ describe('/videos/:id', () => {
   afterEach(disconnectDatabase);
 
   it('shows the video with the given id', async () => {
-    const video = await Video.create({ title: 'new video title', description: 'amazing video description' });
+    const video = await Video.create({ title: 'new video title', description: 'amazing video description', url: 'https://example.com/some-video' });
     const response = await request(app).get(`/videos/${video._id}`)
 
     assert.include(response.text, video.title);
+    assert.include(response.text, video.description);
+    assert.equal(jsdom(response.text).querySelector('iframe.video-player').attributes['src'].textContent, video.url);
   });
 });
